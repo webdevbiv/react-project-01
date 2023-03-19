cdbba5c567bea701f86ceccbb25c00f1b182c341
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-03-19 17:56:21 +0200

    structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,8 +18,17 @@ import {
 function App() {
     return (
         <div className="App">
-            <div>
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
             </div>
+            <Brand />
+            <What />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     )
 }
