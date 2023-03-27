bceb21b7b8353060f9bcb62eb1a862abbd0261ab
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-03-27 13:46:54 +0300

    header and brand sections

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-import './Brand.module.scss'
+import { google, slack, atlassian, dropbox, shopify } from './imports';
+import s from './Brand.module.scss'
+
+
+
 
 function Brand() {
     return (
-        <div>Brand</div>
+        <div className={`${s.gpt3__brand} section__padding`}>
+            <div>
+                <img src={google} alt='brand logo' />
+            </div>
+            <div>
+                <img src={slack} alt='brand logo' />
+            </div>
+            <div>
+                <img src={atlassian} alt='brand logo' />
+            </div>
+            <div>
+                <img src={dropbox} alt='brand logo' />
+            </div>
+            <div>
+                <img src={shopify} alt='brand logo' />
+            </div>
+        </div>
     )
 }
 
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -7,20 +7,24 @@ function Header() {
     return (
         <div className={`${s.gpt3__header} section__padding`} id='home'>
             <div className={`${s.gpt3__header_content}`}>
-                <h1 className={`${s.gpt3__header} gradient__text`}>
-                    Let’s Build Somethingamazing with GPT-3 OpenAI
-                </h1>
-                <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+
+                <h1 className={`gradient__text`}>Let’s Build Something amazing with GPT-3 OpenAI</h1>
+                <p>
+                    Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
+                </p>
+
                 <div className={`${s.gpt3__header_content__input}`}>
                     <input type='email' placeholder='Your Email Address' />
                     <button type='button'>Get Started</button>
                 </div>
-                <div className={`${s.gpt3__header_content__people}`}></div>
-                <img src={people} alt='people' />
-                <p>1,600 people requested access a visit in last 24 hours</p>
-                <div className={`${s.gpt3__header_image}`}>
-                    <img src={ai} alt='ai' />
+
+                <div className={`${s.gpt3__header_content__people}`}>
+                    <img src={people} alt='people' />
+                    <p>1,600 people requested access a visit in last 24 hours</p>
                 </div>
+            </div>
+            <div className={`${s.gpt3__header_image}`}>
+                <img src={ai} alt='ai' />
             </div>
         </div>
     )
